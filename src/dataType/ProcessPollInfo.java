4e719f70d3af2d6427a99fbdcb05763621ae0cba
Hayley Clarke <hayley.vmclarke@gmail.com>
2016-11-28 17:30:11 -0400

    updated proccess poll info - still needs work

--- src/dataType/ProcessPollInfo.java
+++ src/dataType/ProcessPollInfo.java
@@ -1,5 +1,17 @@
 package dataType;
 
 public class ProcessPollInfo {
-
+  private PollInfo pollInfo;
+  
+  private void resetPollInfo(){
+    pollInfo.destination 
+  }
+  
+  private void updatePollInfo(Destination dest){
+    pollInfo.destination = dest;
+  }
+  
+  private String toString(){
+    return (pollInfo.toString());
+  }
 }
