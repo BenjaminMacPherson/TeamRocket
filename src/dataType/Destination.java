469f25b98f27429f245f43817d595f24cef5846f
BenjaminMacPherson <bmacpher@unb.ca>
2016-11-29 12:15:26 -0500

    Update Destination.java

--- src/dataType/Destination.java
+++ src/dataType/Destination.java
@@ -4,11 +4,13 @@ public class Destination {
   private LatLng latitude;
   private LatLng longitude;
   private String destinationName;
+  private int voteCount;
   
   private Destination(String name, LatLng lat, LatLng lon){
     destinationName = this.name;
     latitude = this.lat;
     longitude = this.lon;
+    voteCount = 0;
   }
   
 }
