64b14d305e364dbf34b1d9f7e018e9dd0eab6a6f
BenjaminMacPherson <bmacpher@unb.ca>
2016-11-29 12:08:44 -0500

    Update Destination.java

--- src/dataType/Destination.java
+++ src/dataType/Destination.java
@@ -3,8 +3,10 @@ package dataType;
 public class Destination {
   private LatLng latitude;
   private LatLng longitude;
+  private String destinationName;
   
-  private Destination(LatLng lat, LatLng lon){
+  private Destination(String name, LatLng lat, LatLng lon){
+    destinationName = this.name;
     latitude = this.lat;
     longitude = this.lon;
   }
