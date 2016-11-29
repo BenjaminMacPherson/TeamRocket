279156508fc4bc4587e9d290e84b7dcd8369d46f
BenjaminMacPherson <bmacpher@unb.ca>
2016-11-29 12:14:27 -0500

    Update PollInfo.java

--- src/dataType/PollInfo.java
+++ src/dataType/PollInfo.java
@@ -4,7 +4,9 @@ public class PollInfo {
   Private Destination[] destinationArray;
   
   private void resetPollResults(){
-    
+    for(int i = 0; i<destinationArray.length(); i++){
+      destinationArray[i].count = 0;
+    }
   }
   
   private String toString(){
