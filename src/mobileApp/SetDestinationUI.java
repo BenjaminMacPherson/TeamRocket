51b315707403ef186fc5996f8866ffbe82b6ad01
BenjaminMacPherson <bmacpher@unb.ca>
2016-11-29 12:33:42 -0500

    Update SetDestinationUI.java

--- src/mobileApp/SetDestinationUI.java
+++ src/mobileApp/SetDestinationUI.java
@@ -1,5 +1,12 @@
 package mobileApp;
 
 public class SetDestinationUI {
+  SetDestinationControl SDC = new SetDestinationControl();
+  
+  public void control(){
+    SDC.control();
+  }
+  
+  
 
 }
