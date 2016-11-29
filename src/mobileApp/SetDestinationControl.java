178506fa62cca1a757a7850683f94d6709056536
BenjaminMacPherson <bmacpher@unb.ca>
2016-11-29 12:36:22 -0500

    Update SetDestinationControl.java

--- src/mobileApp/SetDestinationControl.java
+++ src/mobileApp/SetDestinationControl.java
@@ -1,5 +1,13 @@
 package mobileApp;
 
+import DataType.ProcessDroneInfo;
+import DataType.Destination;
+
 public class SetDestinationControl {
 
+  private ProcessDroneInfo PDI = new ProcessDroneInfo();
+  
+  private void control(Destination d){
+    PDI.
+  }
 }
