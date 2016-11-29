afc5919ed68ddf203fb3670a1d44a6d9952d18b9
BenjaminMacPherson <bmacpher@unb.ca>
2016-11-29 12:42:18 -0500

    Update SetDestinationControl.java

--- src/mobileApp/SetDestinationControl.java
+++ src/mobileApp/SetDestinationControl.java
@@ -1,6 +1,8 @@
 package mobileApp;
 
 import DataType.ProcessDroneInfo;
+
+// idk if we need to import destination because it's being sent in from the UI. I also don't know anything about coding anyways
 import DataType.Destination;
 
 public class SetDestinationControl {
@@ -8,6 +10,6 @@ public class SetDestinationControl {
   private ProcessDroneInfo PDI = new ProcessDroneInfo();
   
   private void control(Destination d){
-    PDI.
+    PDI.setDestination(d);
   }
 }
