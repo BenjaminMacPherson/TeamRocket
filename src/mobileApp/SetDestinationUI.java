f10e44a7385be84e128c77f6d434bc4b60d9c798
BenjaminMacPherson <bmacpher@unb.ca>
2016-11-29 12:38:37 -0500

    Update SetDestinationUI.java

--- src/mobileApp/SetDestinationUI.java
+++ src/mobileApp/SetDestinationUI.java
@@ -1,10 +1,13 @@
 package mobileApp;
 
+import DataType.Destination;
+
 public class SetDestinationUI {
   SetDestinationControl SDC = new SetDestinationControl();
+  Destination d = new Destination(); //not really sure if this is the correct way to go about it. Double check to make sure
   
   public void control(){
-    SDC.control();
+    SDC.control(d);
   }
   
   
