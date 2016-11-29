9b604f26fbf4dfdd97c0f796a1a91b3e70c2f190
BenjaminMacPherson <bmacpher@unb.ca>
2016-11-29 12:30:06 -0500

    Update ProcessDroneInfo.java

--- src/dataType/ProcessDroneInfo.java
+++ src/dataType/ProcessDroneInfo.java
@@ -3,11 +3,13 @@ package dataType;
 public class ProcessDroneInfo {
   DroneInfo droneInfo;
   
+  //maybe we should just change this to toString()
   private void getDroneInfo(){
-    
+    System.out.println("Drone's current location: " + droneInfo.currentLocation);
+    System.out.println("Drone's destination: " + droneInfo.destination);
   }  
 
-  private void SetDestination(){
-    
+  private void SetDestination(destination d){
+    droneInfo.destination = d;
   }
 }
